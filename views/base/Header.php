804a3194a6cd5914d12f3f265c6b6272f1086850
jezyq14 <michalek.muzyka@gmail.com>
2025-04-12 23:03:00 +0200

    Add cat facts feature and update header navigation
    - Implemented a random cat fact display on the main page by reading from a new 'catfacts.txt' file.
    - Created a new landing section with a background image and a call-to-action button.
    - Added a "Fact of the Day" section to showcase a random cat fact.
    - Developed a responsive header component with a navigation drawer for better user experience.
    - Included links to facts, blog, and photo sections in the header and sidebar.

---
+++ views/base/Header.php
@@ -0,0 +1,41 @@
+<div class="drawer z-20">
+    <input id="nav-drawer" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col">
+        <!-- Navbar -->
+        <div class="navbar bg-base-300 w-fullpx-8 md:px-20">
+            <div class="flex-none lg:hidden">
+                <label for="nav-drawer" aria-label="open sidebar" class="btn btn-square btn-ghost">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        class="inline-block h-6 w-6 stroke-current">
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                </label>
+            </div>
+            <div class="mx-2 flex-1 text-2xl md:text-3xl font-bold">Kotki</div>
+            <div class="hidden flex-none lg:block">
+                <ul class="menu menu-horizontal">
+                    <!-- Navbar menu content here -->
+                    <li><a href="/facts">Ciekawostki</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                    <li><a href="/photos">Zdjęcia</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="drawer-side z-20">
+        <label for="nav-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+        <ul class="menu bg-base-200 min-h-full w-80 p-4">
+            <!-- Sidebar content here -->
+            <li><a href="/facts">Ciekawostki</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/photos">Zdjęcia</a></li>
+        </ul>
+    </div>
+</div>
--- views/main/MainPage.php
+++ views/main/MainPage.php
@@ -1,5 +1,7 @@
 <?php
+$catFacts = file('catfacts.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
+$randomFact = $catFacts[array_rand($catFacts)];
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +16,24 @@
 </head>
 
 <body>
+    <?php require 'views/base/Header.php' ?>
+
+    <!-- Landing section -->
+    <section class="relative bg-[url(/public/images/cat.avif)] bg-cover bg-center h-[80vh] flex items-center justify-center">
+        <div class="absolute w-[100vw] h-[80vh] bg-[#000] opacity-30"></div>
+        <div class="form-control flex flex-col items-center justify-center gap-2 md:gap-4 z-10 text-base-100">
+            <h1 class="text-4xl md:text-6xl font-bold z-10">Kotki</h1>
+            <p class="text-xl md:text-2xl text-center font-medium">Znajdziesz u nas wszystko, co ważne<br>o naszych ulubionych milutkich kotkach!</p>
+            <a class="btn" href="#fact-of-the-day">Ciekawostka dnia</a>
+        </div>
+    </section>
+
+    <!-- Fact of the day section -->
+    <section class="mx-8 md:mx-20 my-8 flex flex-col gap-4" id="fact-of-the-day">
+        <h2 class="square-header text-4xl font-bold">Ciekawostka Dnia</h2>
+        <p class="md:text-xl"><?php echo $randomFact; ?></p>
+        <a href="/facts" class="btn max-w-fit">Więcej ciekawostek</a>
+    </section>
 
 </body>
 
