81004c7370df5fe6d01ed888d095d8612d8b4c2d
jezyq14 <michalek.muzyka@gmail.com>
2025-04-13 18:17:42 +0200

    Add footer component with copyright information

---
+++ views/base/Footer.php
@@ -0,0 +1,5 @@
+<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+    <aside>
+        <p>Copyright © <?php echo date("Y"); ?> - Michał Muzyka</p>
+    </aside>
+</footer>
