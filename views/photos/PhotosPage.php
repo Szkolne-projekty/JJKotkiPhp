e32c7ec6a29b6fc490a10af200e3622058d0940e
jezyq14 <michalek.muzyka@gmail.com>
2025-04-13 21:43:05 +0200

    Add PhotosPage to display photo gallery

---
+++ views/photos/PhotosPage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require 'views/HtmlLib.php' ?>
+</head>
+
+<body>
+    <?php require 'views/base/Header.php'; ?>
+
+    <section class="mx-8 md:mx-20 my-8 flex flex-col gap-4">
+        <h1 class="square-header text-3xl md:text-4xl font-bold">Galeria zdjęć</h1>
+    </section>
+
+    <?php require 'views/base/Footer.php' ?>
+</body>
+
+</html>
