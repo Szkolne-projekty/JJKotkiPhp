020f3bef5c80406221d63574d25449d4bee1d882
jezyq14 <michalek.muzyka@gmail.com>
2025-04-14 08:55:50 +0200

    Add image gallery to PhotosPage and update Footer component positioning

--- views/base/Footer.php
+++ views/base/Footer.php
@@ -1,4 +1,4 @@
-<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 absolute">
     <aside>
         <p>Copyleft 🄯 <?php echo date("Y"); ?> - Michał Muzyka</p>
     </aside>
--- views/photos/PhotosPage.php
+++ views/photos/PhotosPage.php
@@ -1,3 +1,9 @@
+<?php
+
+$images = glob("public/images/cats/*.*");
+
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 
@@ -12,6 +18,14 @@
 
     <section class="mx-8 md:mx-20 my-8 flex flex-col gap-4">
         <h1 class="square-header text-3xl md:text-4xl font-bold">Galeria zdjęć</h1>
+
+        <div class="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-2">
+            <?php foreach ($images as $image) : ?>
+                <div class="break-inside-avoid mb-2 rounded-lg shadow-lg overflow-hidden">
+                    <img src="/<?= $image ?>" alt="Kotek" class="w-full h-auto">
+                </div>
+            <?php endforeach; ?>
+        </div>
     </section>
 
     <?php require 'views/base/Footer.php' ?>
