b2ec66bc7a0852e4338856c303096c611843a90f
jezyq14 <michalek.muzyka@gmail.com>
2025-04-14 20:39:37 +0200

    Add image gallery to MainPage and update fact display format

--- views/main/MainPage.php
+++ views/main/MainPage.php
@@ -1,7 +1,29 @@
 <?php
+
 $catFacts = file('catfacts.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 $randomFact = $catFacts[array_rand($catFacts)];
+
+$images = file('imagesForLanding.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$randomImages = [];
+if (count($images) >= 4) {
+    $randomKeys = array_rand($images, 4);
+    $randomImages = array_map(function ($key) use ($images) {
+        return $images[$key];
+    }, $randomKeys);
+} else {
+    $randomImages = $images;
+}
+
+/* usort($randomImages, function ($a, $b) {
+    return getimagesize($b)[1] <=> getimagesize($a)[1];
+});
+
+$tallestImage = array_shift($randomImages);
+shuffle($randomImages);
+array_unshift($randomImages, $tallestImage); */
+
 ?>
 
 <!DOCTYPE html>
@@ -23,15 +45,30 @@ $randomFact = $catFacts[array_rand($catFacts)];
         <div class="form-control flex flex-col items-center justify-center gap-2 md:gap-4 z-10 text-base-100">
             <h1 class="text-4xl md:text-6xl font-bold z-10">Kotki</h1>
             <p class="text-xl md:text-2xl text-center font-medium">Znajdziesz u nas wszystko, co ważne<br>o naszych ulubionych milutkich kotkach!</p>
-            <a class="btn" href="#fact-of-the-day">Ciekawostka dnia</a>
+            <div class="flex items-center justify-center""><a class=" btn" href="#fact-of-the-day">Ciekawostka dnia</a></div>
         </div>
     </section>
 
     <!-- Fact of the day section -->
     <section class="mx-8 md:mx-20 my-8 flex flex-col gap-4" id="fact-of-the-day">
         <h2 class="square-header text-3xl md:text-4xl font-bold">Ciekawostka dnia</h2>
-        <p class="md:text-xl"><?php echo $randomFact; ?></p>
-        <a href="/facts" class="btn max-w-fit">Więcej ciekawostek</a>
+        <ul class="list-disc list-inside ml-5 md:ml-7">
+            <li class="md:text-xl list-style"><?php echo $randomFact; ?></li>
+        </ul>
+        <a href=" /facts" class="btn max-w-fit">Więcej ciekawostek</a>
+    </section>
+
+    <!-- Gallery section -->
+    <section class="mx-8 md:mx-20 my-8 flex flex-col gap-4">
+        <h2 class="square-header text-3xl md:text-4xl font-bold">Galeria zdjęć</h2>
+        <div class="columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-2">
+            <?php foreach ($randomImages as $image) : ?>
+                <div class="image-container break-inside-avoid mb-2 rounded-lg shadow-lg overflow-hidden">
+                    <img src="/<?= $image ?>" alt="Kotek" class="w-full h-auto">
+                </div>
+            <?php endforeach; ?>
+        </div>
+        <div class="flex items-center justify-center"><a href=" /photos" class="btn max-w-fit">Więcej zdjęć</a></div>
     </section>
 
     <?php require 'views/base/Footer.php' ?>
