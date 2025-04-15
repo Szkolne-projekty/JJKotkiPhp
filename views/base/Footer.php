05c614035158bbf452c89e3a6c7f36fbe7cf2e4c
jezyq14 <michalek.muzyka@gmail.com>
2025-04-15 17:28:05 +0200

    Refactor footer to use a link for copyleft notice and remove unnecessary aside element

--- views/base/Footer.php
+++ views/base/Footer.php
@@ -1,5 +1,3 @@
-<footer class="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
-    <aside>
-        <p>Copyleft 🄯 <?php echo date("Y"); ?> - Michał Muzyka</p>
-    </aside>
+<footer class="footer sm:footer-horizontal footer-center bg-base-300 p-4">
+    <a href="https://github.com/Szkolne-projekty/JJKotkiPhp" target="_blank" class="link link-hover">Copyleft 🄯 <?php echo date("Y"); ?> - Michał Muzyka</a>
 </footer>
