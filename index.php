f9fbf921e011b70d08764fe62deabe2217a24690
jezyq14 <michal.muzyka@proton.me>
2025-04-22 21:09:12 +0200

    Add error reporting configuration and create PostPage template

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(0);
+ini_set('display_errors', 0);
+
 session_start();
 
 require_once __DIR__ . '/lib/Database.php';
@@ -12,22 +15,22 @@ require_once __DIR__ . '/router.php';
 // ##################################################
 // Routy z GET
 
-// Index
+/* Index */
 get('/', 'views/main/MainPage.php');
 
-// Strona z ciekawostkami
+/* Strona z ciekawostkami */
 get('/facts', 'views/facts/FactsPage.php');
 
-// Strona z galerią zdjęć
+/* Strona z galerią zdjęć */
 get('/photos', 'views/photos/PhotosPage.php');
 
-// Strona logowania
+/* Strona logowania */
 get('/login', 'views/auth/LoginPage.php');
 
-// Strona rejestracji
+/* Strona rejestracji */
 get('/register', 'views/auth/RegisterPage.php');
 
-// Wylogowanie
+/* Wylogowanie */
 get('/logout', function () {
 
     unset($_SESSION);
@@ -37,17 +40,23 @@ get('/logout', function () {
     exit();
 });
 
+/* Posty */
 get('/post/create', 'views/post/CreatePostPage.php');
+get('/post/edit/$id', 'views/post/EditPostPage.php');
+get('/post/delete/$id', 'views/post/DeletePostPage.php');
 
+get('/post/$id', 'views/post/PostPage.php');
 
 // ##################################################
 // ##################################################
 // ##################################################
 // Routy z POST
-post('/login', '/api/login.php');
 
+/* Logowanie */
+post('/login', '/api/login.php');
 post('/register', '/api/register.php');
 
+/* Posty */
 post('/post/create', '/api/createPost.php');
 post('/post/edit', '/api/editPost.php');
 post('/post/delete', '/api/deletePost.php');
---
+++ views/post/PostPage.php
@@ -0,0 +1,31 @@
+<?php
+global $pdo;
+
+require 'lib/Markdown.php';
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require 'views/HtmlLib.php'; ?>
+</head>
+
+<body>
+    <?php require 'views/base/Header.php'; ?>
+
+    <!-- Tytuł i takie tam -->
+    <section class=""></section>
+
+    <!-- Zawartość postu -->
+    <section class=""></section>
+
+    <?php require 'views/base/Footer.php'; ?>
+</body>
+
+</html>
