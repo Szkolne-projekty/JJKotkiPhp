98ef7969732dc04861dfc3798a8974f61a26d96b
jezyq14 <michal.muzyka@proton.me>
2025-04-22 14:34:05 +0200

    Add favicon link to HtmlLib.php

--- views/HtmlLib.php
+++ views/HtmlLib.php
@@ -1,4 +1,7 @@
 <title>Kotki</title>
 
+<!-- Favicon -->
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
+
 <!-- Import wyjściowego cssa z tailwinda -->
 <link rel="stylesheet" href="/public/css/output.css">
