e4bd9922c54e276406942780d9df159ee30d33ab
jezyq14 <michal.muzyka@proton.me>
2025-04-23 16:03:43 +0200

    Enhance PostPage layout and date formatting for better presentation

--- views/post/PostPage.php
+++ views/post/PostPage.php
@@ -3,7 +3,37 @@ global $pdo;
 
 require 'lib/Markdown.php';
 
+$stmt = $pdo->prepare("SELECT p.title, p.image, UNIX_TIMESTAMP(p.created_at) as created_at, u.display_name as author FROM posts p INNER JOIN users u ON p.author = u.id WHERE p.id = :id");
+$stmt->execute(['id' => $id]);
+$post = $stmt->fetch(PDO::FETCH_ASSOC);
+if (!$post) {
+    Utils::redirect('/');
+    exit;
+}
 
+$date = new DateTime();
+$date->setTimestamp($post["created_at"]);
+
+$polskieMiesiace = [
+    1 => 'stycznia',
+    2 => 'lutego',
+    3 => 'marca',
+    4 => 'kwietnia',
+    5 => 'maja',
+    6 => 'czerwca',
+    7 => 'lipca',
+    8 => 'sierpnia',
+    9 => 'września',
+    10 => 'października',
+    11 => 'listopada',
+    12 => 'grudnia'
+];
+
+$day = $date->format('j');
+$month = $date->format('n');
+$year = $date->format('Y');
+
+$dateString = "$day {$polskieMiesiace[$month]} $year";
 
 ?>
 
@@ -20,12 +50,24 @@ require 'lib/Markdown.php';
     <?php require 'views/base/Header.php'; ?>
 
     <!-- Tytuł i takie tam -->
-    <section class=""></section>
+    <section class="md:mx-20 max-h-80 md:max-h-[500px] relative">
+        <img src="/<?php echo $post['image'] ?>" class="max-h-80 md:max-h-[500px] w-full object-cover" alt="Zdjęcie postu">
+
+        <div class="absolute bottom-0 left-[50%] transform -translate-x-[50%] bg-base-100 text-center w-[90%] md:w-[80%] p-4">
+            <h1 class="text-xl md:text-4xl font-bold"><?php echo $post['title']; ?></h1>
+
+            <div class="pt-4 opacity-70 text-xs ">
+                <span><?php echo $post['author']; ?></span>
+                <span class="px-1">·</span>
+                <span><?php echo $dateString; ?></span>
+            </div>
+        </div>
+    </section>
 
     <!-- Zawartość postu -->
     <section class=""></section>
 
-    <?php require 'views/base/Footer.php'; ?>
+    <div class="fixed bottom-0 left-0 w-full"><?php require 'views/base/Footer.php'; ?></div>
 </body>
 
 </html>
