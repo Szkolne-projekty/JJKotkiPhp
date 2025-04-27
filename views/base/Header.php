83a91cecf00dad4c3f192ba7aa49d2187c9188c6
jezyq14 <michal.muzyka@proton.me>
2025-04-27 17:37:05 +0200

    Add profile link to header for logged-in users

--- views/base/Header.php
+++ views/base/Header.php
@@ -35,6 +35,8 @@ $isLoggedIn = Utils::isLoggedIn();
                     <li><a href="/photos">Zdjęcia</a></li>
                     <?php if (!$isLoggedIn): ?>
                         <li><a href="/login">Zaloguj się</a></li>
+                    <?php else: ?>
+                        <li><a href="/profile">Profil</a></li>
                     <?php endif; ?>
                 </ul>
             </div>
@@ -58,6 +60,16 @@ $isLoggedIn = Utils::isLoggedIn();
                             <circle cx="12" cy="7" r="4" />
                         </svg>
                         Zaloguj się
+                    </a>
+                </div>
+            <?php else: ?>
+                <div class="p-4">
+                    <a href="/profile" class="btn btn-block">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user">
+                            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
+                            <circle cx="12" cy="7" r="4" />
+                        </svg>
+                        Profil
                     </a>
                 </div>
             <?php endif; ?>
